a84d285d5c1547236af6346117ac63125708223a
Jeremy Gorniak <railspider@gmail.com>
2022-03-10 22:49:52 +1100

    Need to setup crud views and urls

--- htmxdjango_project/settings.py
+++ htmxdjango_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y2jo)ko=1256+c3nv2ye$$#(rt1eehoacj41p84*z$x3o=^o68'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

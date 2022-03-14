08cb4bae60887ce7b7d0a1448935b759bf6eb128
Jeremy Gorniak <railspider@gmail.com>
2022-03-14 11:47:28 +1100

    Need to fix form PUT request

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from crud.views import live_edit
+from crud.views import live_edit, edit_person, get_person, update_person
 
 urlpatterns = [
     path('', live_edit, name="live_edit"),
+    path("edit/<int:pk>/", edit_person, name="edit_person"),
+    path("person/<int:pk>/", get_person, name="get_person"),
+    path("update/<int:pk>/", update_person, name="update_person"),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
 from crud.models import Person
 
 # Create your views here.
@@ -6,3 +8,26 @@ def live_edit(request):
     people = Person.objects.all()
     
     return render(request, 'crud/live_edit.html', {'people': people})
+
+@require_http_methods(['GET'])
+def get_person(request, pk):
+    person = Person.objects.get(pk=pk)
+    
+    return render(request, 'crud/partials/person.html', {'person': person})
+
+@require_http_methods(['GET'])
+def edit_person(request, pk):
+    person = Person.objects.get(pk=pk)
+    
+    return render(request, 'crud/partials/editable.html', {'person': person})
+
+@require_http_methods(['PUT'])
+def update_person(request, pk):
+    person = Person.objects.get(pk=pk)
+    
+    person.first_name = request.PUT['firstName']
+    person.last_name = request.PUT['lastName']
+    person.email_address = request.PUT['email']
+    person.save()
+    
+    return render(request, 'crud/partials/person.html', {'person': person})

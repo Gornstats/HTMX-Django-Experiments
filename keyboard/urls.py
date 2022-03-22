b0a0f7f45da85fe8c4066f7de99589d35049edf2
Jeremy Gorniak <railspider@gmail.com>
2022-03-22 21:50:40 +1100

    Completed colour changing buttons and keys

--- keyboard/urls.py
+++ keyboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from keyboard.views import button_press
+from keyboard.views import button_press, change_colour
 
 urlpatterns = [
     path('', button_press, name="button_press"),
+    path("change/<str:colour>/", change_colour, name="change_colour"),
 ]
--- keyboard/views.py
+++ keyboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http.response import HttpResponse
 
 # Create your views here.
 def button_press(request):
     return render(request, 'keyboard/button_press.html')
+
+@require_http_methods(['POST'])
+def change_colour(request, colour):
+    return HttpResponse("<div id='colourBox' style='background-color:" + colour + ";'></div>")

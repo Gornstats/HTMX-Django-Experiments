33badd940c7b78fb1da47efa026aa04b8fb97d4c
Jeremy Gorniak <railspider@gmail.com>
2022-03-30 22:07:09 +1100

    HTMX routes for live updating certificate

--- certificate/urls.py
+++ certificate/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from certificate.views import certificate_builder
+from certificate.views import certificate_builder, edit_name, edit_qual, edit_date, edit_sign
 
 urlpatterns = [
     path('', certificate_builder, name="certificate_builder"),
+    path('name/', edit_name, name="edit_name"),
+    path('qual/', edit_qual, name="edit_qual"),
+    path('date/', edit_date, name="edit_date"),
+    path('sign/', edit_sign, name="edit_sign"),
 ]
--- certificate/views.py
+++ certificate/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.utils.html import escape
 
 # Create your views here.
 def certificate_builder(request):
     return render(request, "certificate/certificate.html")
+
+def edit_name(request):
+    name = escape(request.POST.get('name','')) 
+    return HttpResponse(name)
+
+def edit_qual(request):
+    qual = escape(request.POST.get('qual',''))
+    return HttpResponse(qual)
+
+def edit_date(request):
+    date = escape(request.POST.get('awardDate','')) 
+    return HttpResponse(date)
+
+def edit_sign(request):
+    sign = escape(request.POST.get('signature','')) 
+    return HttpResponse(sign)

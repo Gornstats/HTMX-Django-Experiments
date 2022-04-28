b14ab41dd85bddb23a0d9484a7be562c1c1f6093
Jeremy Gorniak <railspider@gmail.com>
2022-04-28 20:29:31 +1000

    Added Pet model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    pet_name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.pet_name

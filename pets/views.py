72275e748c4241061afe1c4046fb326fec6ec6b6
Jeremy Gorniak <railspider@gmail.com>
2022-05-22 11:59:25 +1000

    Updated templates to include Tailwind.css

--- pets/views.py
+++ pets/views.py
@@ -4,5 +4,6 @@ from pets.models import Pet
 # Create your views here.
 def pet_list(request):
     pets = Pet.objects.all()
+    context = {'pets': pets,}
     
-    return render(request, 'pets/pet_list.html', {'pets': pets,})
+    return render(request, 'pets/pet_list.html', context)

4568a77e3f30d9ece411dbf1b9cf34d416a593c6
Jeremy Gorniak <railspider@gmail.com>
2022-05-29 11:00:36 +1000

    Added dropdowns

--- dropdowns/urls.py
+++ dropdowns/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from dropdowns.views import index
+from dropdowns.views import index, options
 
 urlpatterns = [
     path('', index, name="index"),
+    path('options', options, name="options"),
 ]
--- dropdowns/views.py
+++ dropdowns/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'dropdowns/dropdowns.html')
 
+def options(request):
+    animal = request.GET.get('animal')
+    context ={'animal': animal,}
+    
+    return render(request, 'dropdowns/options.html', context)

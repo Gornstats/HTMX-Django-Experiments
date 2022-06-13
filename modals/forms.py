3f8b640c72f4adf8d9072d1ffa87f3d3164eba34
Jeremy Gorniak <railspider@gmail.com>
2022-06-13 21:10:13 +1000

    Updated form validation

--- modals/forms.py
+++ modals/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from modals.models import Feedback
 
 class FeedbackForm(forms.ModelForm):
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email.startswith('c'):
+            raise forms.ValidationError("Email can't start with c")
+        return email
+        
     class Meta:
         model = Feedback
         fields = ['message', 'email']

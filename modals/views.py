9f50d4af47eff8026cd74954cbdbc0d784eae540
Jeremy Gorniak <railspider@gmail.com>
2022-06-13 15:40:50 +1000

    Added form result confirmation using HTMX

--- modals/views.py
+++ modals/views.py
@@ -5,7 +5,13 @@ from modals.forms import FeedbackForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'modals/partials/success.html')
+        return render(request, 'modals/partials/failure.html')
+     
     form = FeedbackForm()
-    context = {'form': form}
-    
+    context = {'form': form}   
     return render(request, 'modals/index.html', context)
